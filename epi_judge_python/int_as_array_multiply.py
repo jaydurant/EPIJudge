8aa9d269a2a739e81c11e08929b4a2c631d55c4d
Jason Durant <durantjay1@gmail.com>
2024-01-08 23:51:51 -0500

    add array mult

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -4,34 +4,27 @@ from test_framework import generic_test
 
 
 def multiply(num1: List[int], num2: List[int]) -> List[int]:
-    result_arr = [0] * (len(num1) + len(num2))
-     
-    is_negative = False
-    if num1[0] * num2[0] < 0:
-        is_negative = True
-    num1[0] = abs(num1[0])
-    num2[0] = abs(num2[0])
+    result = [0] * (len(num1) + len(num2))
+    sign = -1 if (num1[0] < 0) ^ (num2[0] < 0) else 1
+    num1[0], num2[0] = abs(num1[0]), abs(num2[0])
 
     num1.reverse()
     num2.reverse()
+
     for ix1, val1 in enumerate(num1):
         for ix2, val2 in enumerate(num2):
-            curr_ix = ix1 + ix2
-            prod = val1 * val2 + result_arr[curr_ix]
-            carry = prod // 10
-            lsd = prod % 10
+            result[ix1 + ix2] += val1 * val2
+            result[ix1 + ix2 + 1] += result[ix1 + ix2] // 10
+            result[ix1 + ix2] %= 10
+    
+    result.reverse()
 
-            result_arr[curr_ix] = lsd
-            result_arr[curr_ix + 1] += carry
+    result = result[next((ix for ix, val in enumerate(result) if val != 0),
+                         len(result)):] or [0]
 
-    while result_arr[-1] == 0 and len(result_arr) > 1:
-        result_arr.pop()
+    result = [sign * result[0]] + result[1:]
 
-    if is_negative:
-        result_arr[-1] *= -1
-    result_arr.reverse()
-
-    return result_arr
+    return result
 
 
 if __name__ == '__main__':
